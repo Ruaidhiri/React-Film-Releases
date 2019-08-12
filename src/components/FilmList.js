8c7c4e7da539b1a3fddad1e35b7bf3dae6d1d932
Rory <ruaidhiri@gmail.com>
2019-08-12 21:51:47 +0100

    Film Releases Homework

---
+++ src/components/FilmList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Film from './Film';
+
+class FilmList extends Component {
+  render() {
+    const filmNodes = this.props.data.map(film => {
+      return(<Film link = {film.url}>{film.name}</Film>)
+    })
+
+    return(
+      <div className = "film-list">
+        {filmNodes}
+      </div>
+    )
+  }
+}
+
+export default FilmList;
